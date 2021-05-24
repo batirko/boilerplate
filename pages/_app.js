379ff4c7de877cd6b5502c710538619588371ba2
batirko <batirko@gmail.com>
2021-05-24 20:44:53 +0300

    Initialisation

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import { StylesProvider } from '@material-ui/core/styles';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StylesProvider injectFirst>
+      <Component {...pageProps} />
+    </StylesProvider>
+  )
 }
 
 export default MyApp
